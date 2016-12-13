05747d501b3db4551aca8fe4b9fcd166d79aa542
Renee <rchar20@gmail.com>
2016-12-12 18:02:42 -1000

    adds mobile styling and 404 styling

--- components/static/NotFound.js
+++ components/static/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from '../sass/notFound.scss';
+import {Link} from 'react-router';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div>
-        <h1 id={styles.title}>404 Page Not Found</h1>
+      <div id={styles.container}>
+        <h1 id={styles.title}>404 Error</h1>
+        <p id={styles.description}>The page you were looking for does not exist.</p>
+        <Link to='/' id={styles.link}>Back</Link>
       </div>
     )
   }
