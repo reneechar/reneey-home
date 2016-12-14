e99af65c83843f940961b8e6ec0c67b3334a4a2f
Renee <rchar20@gmail.com>
2016-12-13 16:02:32 -1000

    adds home page and moves footer down on contact

--- components/static/About.js
+++ components/static/About.js
@@ -9,8 +9,20 @@ class About extends React.Component {
         </div>
         <div id={styles.description}>
           Hello! My name is Renee and I am a Full Stack Web Developer.
-          I am passionate about creating elegant and efficient web
-          applications.
+          My origin as a Web Developer began when I was working as a hospital
+          administrator in Boston. Work was becoming less challenging and
+          I had already taken up the responsibilities of 3 different positions
+          in the clinic. I wanted to gain a technical skill, so I began learning
+          how to code online. Soon after, I realized that the website for
+          my department was outdated and needed improvement. I excitedly began
+          creating my updated version and I displayed it to my supervisor.
+          I was disappointed to hear that I would not be allowed to continue
+          working on it as the hospital had their own Web Development team. From
+          this experiece, I discovered that I had a true passion for creating web
+          applications. Deciding to follow my passion, I quit my job in Boston and moved
+          to Hawaii to study Web Development. I was accepted by DevLeague,
+          Hawaii's premier coding bootcamp, where I underwent 800+ hours of coding
+          within a 3 month timespan.
         </div>
         <div id={styles.tools}>
           <ul id={styles.toolList}>
@@ -27,7 +39,7 @@ class About extends React.Component {
             <li>Sequelize</li>
             <li>Webpack</li>
             <li>Git</li>
-            <li>GitHub</li>
+            <li>Github</li>
             <li>AJAX</li>
             <li>Mocha</li>
             <li>Chai</li>
--- components/static/Home.js
+++ components/static/Home.js
@@ -4,7 +4,17 @@ import styles from '../sass/home.scss';
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div id={styles.container}>
+        <div id={styles.banner}>
+          <div>
+            <h1 id={styles.header}>Always Be Creating</h1>
+          </div>
+        </div>
+        <div id={styles.content}>
+          <div>
+            <h3 id={styles.description}>Hi! My name is Renee and I love creating responsive and functional web applications.</h3>
+          </div>
+        </div>
       </div>
     )
   }
